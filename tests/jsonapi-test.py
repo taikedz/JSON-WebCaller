8a3cfb3ae94e50603cc0cb24767cddf24e56eb12
Tai @ Flex <dch.tai@gmail.com>
2017-01-18 21:22:58 +0000

    make sure all subtitutions are performed

--- tests/jsonapi-test.py
+++ tests/jsonapi-test.py
@@ -1,8 +1,16 @@
 import jsonapi
 import testf
+import console
 
 testf.equal(
     jsonapi.updateurl("http://mock/$id/$ide/$id/thing",{"id":"ME","ide":"vim"}),
     "http://mock/ME/vim/ME/thing",
     "Replace whole-words in URL"
 )
+
+try:
+    jsonapi.updateurl("http://thing/$id", {"not":"applicable"})
+    console.faile("FAIL Prevent unmet substitutions")
+except jsonapi.WebAPIHTTPException:
+    console.infoe("PASS Prevent unmet substitutions")
+    
